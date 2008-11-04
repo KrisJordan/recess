18151975afa6096fd584e67baeeaafd5ad8ba504
Kris Jordan <krisjordan@gmail.com>
2008-11-04 12:15:32 -0500

    Comments on Inflector and some minor fixes

--- lib/recess/lang/Inflector.class.php
+++ lib/recess/lang/Inflector.class.php
@@ -1,40 +1,104 @@
 <?php
-
+/**
+ * The inflector provides basic functionality for transforming words
+ * between their singular and plural forms, as well as programmatic forms
+ * (i.e. camelCapsFormat, ProperCapsFormat, under_scores_format)
+ * 
+ * @author Kris Jordan
+ */
 class Inflector {
+	
+	/**
+	 * Return the plural form of an english word, in many cases.
+	 * Currently this is naive and only appends an 's' to the end of a word.
+	 * i.e. person => persons
+	 *      thing => things
+	 *      goose => gooses
+	 *
+	 * @param string $word
+	 * @return string
+	 */
 	public static function toPlural($word) {
 		return $word .= 's';
 	}
 	
+	/**
+	 * Return the singular form of an english word, in many cases.
+	 * Currently this is naive and only removes the last character
+	 * of a string.
+	 * 
+	 * i.e. persons => persons
+	 * 		things => thing
+	 * 		oxen => oxe
+	 *
+	 * @param string $word
+	 * @return string
+	 */
 	public static function toSingular($word) {
 		return substr($word, 0, -1);
 	}
 	
+	/**
+	 * Return whether or not an english word is plural. Currently
+	 * naive and only returns whether or not the last character
+	 * is an 's' or not.
+	 *
+	 * @param string $word
+	 * @return string
+	 */
 	public static function isPlural($word) {
 		return substr($word, -1, 1) === 's';
 	}
 	
+	/**
+	 * Go from underscores_form or camelCapsForm to ProperCapsForm.
+	 * 
+	 * i.e. this_is_in_underscores => ThisIsInUnderscores
+	 * 		helloWorld => HelloWorld
+	 * 
+	 * @param string $word in camelCaps or underscores_form.
+	 * @return string in ProperCapsForm
+	 */
 	public static function toProperCaps($word) {
 		return ucfirst(self::toCamelCaps($word));
 	}
 	
+	/**
+	 * Go from underscores_form or ProperCapsForm to camelCapsForm
+	 * 
+	 * i.e. this_is_in_underscores => thisIsInUnderscores
+	 * 		HelloWorld => helloWorld
+	 *
+	 * @param string $word in ProperCapsForm or underscores_form
+	 * @return string camelCapsForm
+	 */
 	public static function toCamelCaps($word) {
-		return preg_replace('/_([a-z])/', 'ucfirst(${1})', $word);
+		$word = preg_replace('/_([a-z])/', 'ucfirst(${1})', $word);
+		$word[0] = strtolower($word[0]);
+		return $word;
 	}
 	
-	public static function toEnglish($word) {
-		$word = ucfirst($word);
-		for($i = 0; $i < strlen($word) ; $i++) {
-			if($word[$i] == '_') {
-				$word[$i+1] = strtoupper($word[$i+1]);
-			}
-		}
-		return str_replace('_', ' ', $word);
-	}
-	
+	/**
+	 * Go from ProperCapsForm or camelCapsForm to underscores_form
+	 *
+	 * @param string $word in camelCapsForm or ProperCapsForm
+	 * @return string underscores_form
+	 */
 	public static function toUnderscores($word) {
 		$word[0] = strtolower($word[0]);
 		$word = preg_replace('/([A-Z])/', 'strtolower(${1})_', $word);
 		return $word;
+	}
+	
+	/**
+	 * Go from underscores_form or camelCapsForm or ProperCapsForm to English Form.
+	 *
+	 * @param string $word in underscores_form or camelCapsForm or ProperCapsForm
+	 * @return string in English Form
+	 */
+	public static function toEnglish($word) {
+		$word = preg_replace('/(?:(^.)|_([a-z])|([A-Z]))/', ' strtoupper(${1}${2}${3})', $word);
+		return substr($word, 1);
 	}
 }
 

713c699d523c9d922d93b7d12c268f28cd8d6723
Kris Jordan <krisjordan@gmail.com>
2008-12-24 16:58:18 -0500

    Preparing for PP, removing other apps

--- apps/routingDemo/views/common/navigation.php
+++
@@ -1,8 +0,0 @@
-<div class="span-19 navigation last">
-	<ul>
-		<a href="">Navigation</a>
-		<a href="">Could</a>
-		<a href="">Go</a>
-		<a href="">Here</a>
-	</ul>
-</div>
--- apps/routingDemo/views/home/home.php
+++
@@ -1,6 +0,0 @@
-<?php
-$title = 'Routing Demo';
-include_once($viewsDir . 'common/header.php');
-?>
-
-<?php include_once($viewsDir . 'common/footer.php'); ?>

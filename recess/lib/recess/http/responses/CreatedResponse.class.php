077dc7924358ea6cb9e9f12ebcdef6e1c29a5aee
KrisJordan <krisjordan@gmail.com>
2009-02-04 17:56:47 -0500

    Fix for ticket #61 - improper Location header on a Created Response

--- recess/lib/recess/http/responses/CreatedResponse.class.php
+++ recess/lib/recess/http/responses/CreatedResponse.class.php
@@ -7,7 +7,7 @@ class CreatedResponse extends ForwardingResponse {
 	public function __construct(Request $request, $resourceUri, $contentUri = null) {
 		if(!isset($contentUri)) $contentUri = $resourceUri;
 		parent::__construct($request, ResponseCodes::HTTP_CREATED, $contentUri);
-		$this->addHeader('Location: http://localhost' . $resourceUri); // TODO: This should not reference localhost!
+		$this->addHeader('Location: http://' . $_SERVER['SERVER_NAME'] . $resourceUri);
 	}
 	
 }
